5918d37e29a03ea2b5ef3935bec6d35d8cbdf237
Courtney Jensen <courtneyajensen14@gmail.com>
2023-01-03 00:23:10 -0700

    delete id

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    fs.readFile('/db/db.json', 'utf-8', (err, data) => {
         if (err) throw err;
         res.json(JSON.parse(data));
     });
@@ -21,13 +21,12 @@ app.get('/api/notes', (req, res) => {
 //Post notes and recieve new notes and dataj
 app.post('/api/notes', (req, res) => {
     const data = {
-        id: uniqueid(),
         title: req.body.title,
         text: req.body.text
     };
     console.log(data);
     dbNotes.push(data);
-    fs.writeFile('./db/db.json', JSON.stringify(dbNotes), () => {
+    fs.writeFile('/db/db.json', JSON.stringify(dbNotes), () => {
         res.send('Success!')
     });
 });
@@ -37,14 +36,14 @@ app.delete('/api/notes/:id', (req, res) => {
     const params = req.params.id;
     const deleteArray = dbNotes.filter(arrayContents => arrayContents.id != params);
     console.log(deleteArray);
-    fs.writeFile('./db/db.json', JSON.stringify(deleteArray), () => {
+    fs.writeFile('/db/db.json', JSON.stringify(deleteArray), () => {
         res.send('Deleted successfully!');
     });
 });
 
 //Route index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+    res.sendFile(path.join(_dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () => {
